b667d26dd38816c102404a82e7c134dc0213f181
Noud63 <noudvandun@gmail.com>
2025-12-11 10:24:27 +0100

    remove link

--- app/layout.js
+++ app/layout.js
@@ -16,8 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      
       <body>
         <Header />
         <Providers>{children}</Providers>
