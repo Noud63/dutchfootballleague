5d383d369b94bebe510df69a4f9744fd5b4bf81b
Noud63 <noudvandun@gmail.com>
2025-12-11 17:10:43 +0100

    footer z-index

--- components/ClubInfo.jsx
+++ components/ClubInfo.jsx
@@ -15,19 +15,6 @@ const ClubInfo = () => {
   const { teams } = state;
 
   const [players, setPlayers] = useState([]);
-  // const [showScrollButton, setShowScrollButton] = useState(false);
-
-  // useEffect(() => {
-  //   const handleScrollVisibility = () => {
-  //     if (window.scrollY > 1000) {
-  //       setShowScrollButton(true);
-  //     } else {
-  //       setShowScrollButton(false);
-  //     }
-  //   };
-  //   window.addEventListener("scroll", handleScrollVisibility);
-  //   return () => window.removeEventListener("scroll", handleScrollVisibility);
-  // }, []);
 
   useEffect(() => {
     if (Array.isArray(teams) && teams.length > 0) {
@@ -35,13 +22,6 @@ const ClubInfo = () => {
       setPlayers(squadArrays);
     }
   }, [teams]);
-
-  // const scrollToTop = () => {
-  //   window.scrollTo({ top: 0, behavior: "smooth" });
-  // };
-
-  // console.log("S:", shirts);
-  // console.log("T:", teams);
 
   return (
     <div className={styles.infoContainer}>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,4 @@
-"useClient"
+"useClient";
 import React from "react";
 //import { useWindowWidth } from 'react-window-size-hooks';
 import styles from "../styles/Footer.module.css";
@@ -11,39 +11,40 @@ const Footer = () => {
       <div className={styles.eredivisie}>
         <div className={styles.footerTitleWrapper}>
           <div>
-            <Image src="/home/eredivisielogo_blue.png" width={100} height={0} alt="Eredivisie Logo" className={styles.logo} />
+            <Image
+              src="/home/eredivisielogo_blue.png"
+              width={100}
+              height={0}
+              alt="Eredivisie Logo"
+              className={styles.logo}
+            />
           </div>
           <div>KNVB Eredivisie 2025-2026</div>
         </div>
       </div>
       <div className={styles.footerMenu}>
         <div className={styles.links}>
-    
-                    <Link href="/" className={styles.link}>
-                        <div>Home</div>
-                     </Link>
-    
-                    <Link href="/uitslagen" className={styles.link}>
-                        <div >Wedstrijden</div>
-                    </Link>
-    
-                    <Link href="/recent" className={styles.link}> 
-                        <div >Recent</div>
-                    </Link>
-    
-                    <Link href='/stand' className={styles.link}>
-                        <div >Stand</div>
-                    </Link>
-                    <Link href='/clubinfo' className={styles.link}>
-                        <div >Clubs</div>
-                    </Link>
-    
-    
-                </div>
-      </div>
-      <div className={styles.colofon}>
-        noudvandun&copy;webdevelopment
+          <Link href="/" className={styles.link}>
+            <div>Home</div>
+          </Link>
+
+          <Link href="/uitslagen" className={styles.link}>
+            <div>Wedstrijden</div>
+          </Link>
+
+          <Link href="/recent" className={styles.link}>
+            <div>Recent</div>
+          </Link>
+
+          <Link href="/stand" className={styles.link}>
+            <div>Stand</div>
+          </Link>
+          <Link href="/clubinfo" className={styles.link}>
+            <div>Clubs</div>
+          </Link>
         </div>
+      </div>
+      <div className={styles.colofon}>noudvandun&copy;webdevelopment</div>
     </div>
   );
 };
