df1e741bbf82889b1ec3e45a860b8d2757ac4047
Noud63 <noudvandun@gmail.com>
2025-12-17 14:47:02 +0100

    coaches

---
+++ data/shirtsArray.js
@@ -0,0 +1,22 @@
+const shirts = [
+  { id: 1919, uit: "volendam_uit.jpg", thuis: "volendam_thuis.jpg", trainerImg: "rickkruys.png", naam: "Rick Kruys" },
+  { id: 678, uit: "ajax_uit.jpg", thuis: "ajax_thuis.jpg", trainerImg: "fredgrim.png", naam: "Fred Grim" },
+  { id: 682, uit: "az_uit.jpg", thuis: "az_thuis.jpg", trainerImg: "maartenmartens.png", naam: "Maarten Martens" },
+  { id: 718, uit: "eagles_uit.jpg", thuis: "eagles_thuis.jpg", trainerImg: "melvinboel.png", naam: "Melvin Boel" },
+  { id: 670, uit: "excelsior_uit.jpg", thuis: "excelsior_thuis.jpg", trainerImg: "rubendenuil.png", naam: "Ruben den Uil" },
+  { id: 1920, uit: "fortuna_uit.jpg", thuis: "fortuna_thuis.jpg", trainerImg: "dannybuijs.png", naam: "Danny Buijs" },
+  { id: 677, uit: "groningen_uit.jpg", thuis: "groningen_thuis.jpg", trainerImg: "dicklukkien.png", naam: "Dick Lukkien" },
+  { id: 673, uit: "heerenveen_uit.jpg", thuis: "heerenveen_thuis.jpg", trainerImg: "robinveldman.png", naam: "Robin Veldman" },
+  { id: 671, uit: "heracles_uit.jpg", thuis: "heracles_thuis.jpg", trainerImg: "ernestfaber.png", naam: "Ernest Faber" },
+  { id: 681, uit: "nac_uit.jpg", thuis: "nac_thuis.jpg", trainerImg: "carlhoefkens.png", naam: "Carl Hoefkens" },
+  { id: 1915, uit: "nec_uit.jpg", thuis: "nec_thuis.jpg", trainerImg: "dickschreuder.png", naam: "Dick Schreuder" },
+  { id: 684, uit: "pec_uit.jpg", thuis: "pec_thuis.jpg", trainerImg: "henryvandervegt.png", naam: "Henry van der Vegt" },
+  { id: 674, uit: "psv_uit.jpg", thuis: "psv_thuis.jpg", trainerImg: "peterbosz.png", naam: "Peter Bosz" },
+  { id: 6806, uit: "sparta_uit.jpg", thuis: "sparta_thuis.jpg", trainerImg: "mauricesteijn.png", naam: "Maurice Steijn" },
+  { id: 1912, uit: "telstar_uit.jpg", thuis: "telstar_thuis.jpg", trainerImg: "anthonycorreia.png", naam: "Anthony Correia" },
+  { id: 666, uit: "twente_uit.jpg", thuis: "twente_thuis.jpg", trainerImg: "johnvandenbrom.png", naam: "John van den Brom" },
+  { id: 676, uit: "utrecht_uit.jpg", thuis: "utrecht_thuis.jpg", trainerImg: "ronjans.png", naam: "Ron Jans" },
+  { id: 675, uit: "feyenoord_uit.jpg", thuis: "feyenoord_thuis.jpg", trainerImg: "robinvanpersie.png", naam: "Robin van Persie" },
+];
+
+export default shirts
